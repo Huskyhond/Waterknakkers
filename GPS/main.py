5d83cd48e3039e6201890294b1b5e31c973767f6
Niek Eichner <niek@niekeichner.nl>
2017-04-06 17:04:30 +0200

    Made autorefresher for GPS and a working GPS map

--- GPS/main.py
+++ GPS/main.py
@@ -1,15 +1,29 @@
 import folium
+import time
 coordX = 51.89855
 coordY = 4.418458
 
-map_osm = folium.Map(location=[coordX, coordY], zoom_start=15)
-first = True
-with open('coords.txt') as f:
-    for line in f:
-        x, y = line.split(',')
-        if first:
-            folium.Marker([x, y]).add_to(map_osm)
-            first = False
-        else:
-            folium.CircleMarker(location=[x, y], radius=2, fill_color='#3186cc').add_to(map_osm)
-map_osm.save('osm.html')
+
+def RefreshMap():
+    map_osm = None
+    first = True
+    with open('coords.txt') as f:
+        for line in f:
+            coords = line.split(',')
+            if(len(coords) < 2): 
+                break
+            x = coords[0]
+            y = coords[1]
+            if first:
+                map_osm = folium.Map(location=[x, y], zoom_start=18)
+                folium.Marker([x, y]).add_to(map_osm)
+                first = False
+            else:
+                folium.CircleMarker(location=[x, y], radius=2, fill_color='#3186cc').add_to(map_osm)
+    if map_osm is not None:
+        map_osm.save('osm.html')
+
+while True:
+    RefreshMap()
+    time.sleep(2)
+    print('Refreshed map')
