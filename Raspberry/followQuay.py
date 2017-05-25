4cd44dc5363c02d77975c0dfcb5b9516a6f80040
chinjihoe <chinji.hoe@hotmail.com>
2017-05-25 17:07:39 +0200

    Steer or left recognition

--- Raspberry/followQuay.py
+++ Raspberry/followQuay.py
@@ -2,21 +2,29 @@
 import math
 
 
-sensorDistances = [4,9]
-sensorDegrees = 45
+ #Sensors perpendicular to wall, angled to wall, forward
 
-def calcBoatAngle():
+def calcBoatAngle(sensorDistances, sensorDegrees = 45):
     correctDistance = sensorDistances[0] / math.cos(sensorDegrees)
     errorDistance = sensorDistances[1] - correctDistance
     perpendicular = math.sqrt(math.pow(correctDistance,2) - math.pow(sensorDistances[0],2))
-    wallLength = math.sqrt(math.pow(errorDistance,2) + math.pow(perpendicular,2) -2 * errorDistance * perpendicular * math.cos(math.radians(135)))
-    a = errorDistance * math.sin(math.radians(135))
+    wallLength = math.sqrt(math.pow(errorDistance,2) + math.pow(perpendicular,2) -2 * errorDistance * perpendicular * math.cos(math.radians(180-sensorDegrees)))
+    a = errorDistance * math.sin(math.radians(180-sensorDegrees))
     angle = math.degrees(math.asin(a/wallLength))
-    print(angle)
+    return angle
 
 
+def adjustBoat():
+    boatAngle = calcBoatAngle([2,7,1])
+    print(boatAngle)    
     
+    if boatAngle < 0:
+        print("Steer Right")
+    else:
+        print("Steer Left")
 
-calcBoatAngle()
+
+
+adjustBoat()
 
 
