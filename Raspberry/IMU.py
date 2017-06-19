42e7f0a0054d768a5b667d87553a2a486a3615ad
Boris van Norren <boris0101@hotmail.com>
2017-06-19 14:49:41 +0200

    Updated followCoords
    Cleaned up IMU
    followCoords now works with Boatcontroller

--- Raspberry/IMU.py
+++ Raspberry/IMU.py
@@ -17,29 +17,24 @@ class IMU:
         self.PORT = 4223
         self.UID = "62Bous"
         self.REFRESH_TIME = 50
-        self.prevTime = int(round(time() * 1000))
         self.debug = debug
-        # List with accelerations
-        self.currentTotal = []
-        # DeltaTime
-        self.currentdt = 0
-        # Cycle counter
-        self.i = 0
-        # Velocity
-        self.cvelocity = 0
         self.ipcon = IPConnection()
         self.imu = BrickIMUV2(self.UID, self.ipcon)
 
     def get_orientation(self):
         data = self.imu.get_all_data()
-        H, R, P = data.euler_angle
-        return round(H/16.0, 2)
+        return data
         
     def connect(self):
         # Connect the IP connection to the IMU brick
-        self.ipcon.connect(self.HOST, self.PORT)
+        try:
+            self.ipcon.connect(self.HOST, self.PORT)
+        except Exception:
+            print("Can't connect to the IMU brick")
+            return False
         # Refresh the timer of the Callback to REFRESH_TIME
         self.imu.set_all_data_period(self.REFRESH_TIME)
+        return True
 
     def disconnect(self):
         self.ipcon.disconnect()
--- Raspberry/followCoords.py
+++ Raspberry/followCoords.py
@@ -12,19 +12,24 @@ import numpy as np
 import math
 import os
 
-class Coords():
-    # START_COORD = [51.89883324259825, 4.4081997871398935]
-    # GOAL = [51.8983036195826, 4.405646324157716]
-    
-    START_COORD = [53, 5]
-    GOAL = [[55, 6], [55,4], [51,4], [51,6]]
+# START_COORD = [53, 5]
+# GOAL = [[53, 5], [54, 4], [54,6], [52,4], [52,6]]
 
+class Coords():
     def __init__(self, callback, max_power, goal, debug = False):
+        """
+        This class performs GPS-to-GPS navigation with the Tinkerforge IMU v2.0
+        :param callback: Callback function that returns the driveValues of the boat
+        :param max_power: The boat's max_power.
+        :param goal: Array of coordinates with the first input being the boat own coordinates
+        :param debug: If debug is true, it will print most values. Default = False
+        """
         self.imu = IMU()
-        self.imu.connect()
+        self.connected = self.imu.connect()
         self.coordinates = [0,0]
         self.goalNumber = 1
-        self.goal = goal[goalNumber]
+        self.totalGoal = goal
+        self.goal = self.totalGoal[self.goalNumber]
         self.max_power = max_power/100
         self.debug = debug
         self.t = None
@@ -50,18 +55,19 @@ class Coords():
         out_min *= 100
         out_max *= 100
         res = np.clip( ((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min) , out_min, out_max)
+        return float(res)/100
 
     # Calculate the Angle to boat has to rotate to in order to reach the goal.
     # The calculation is based on a 360 circle and the goal being in range 1-360 with 360 being North
     def calcGoalAngle(self):
         dLng = self.goal[0] - self.coordinates[0]
         dLat = self.goal[1] - self.coordinates[1]
+        goalAngle = 0
         
         East = 0 if dLng < 0 else 1
         North = 0 if dLat < 0 else 1
         
         if(North == 1 & East == 1):
-            print("test1")
             goalAngle = math.degrees(math.atan(dLng / dLat))
         elif(North == 0 & East == 1):
             goalAngle = math.degrees(math.atan(dLat / dLng)) + 90
@@ -73,12 +79,7 @@ class Coords():
 
     # Function to rotate the boat
     def rotateBoat(self):
-        if(self.debug):
-            os.system('cls')
-        self.boatAngle = self.imu.get_orientation()
         motorL = motorR = rudder = 0
-        if(self.goalAngle is None):
-            self.goalAngle = self.calcGoalAngle()
 
         clock = True
 
@@ -92,17 +93,17 @@ class Coords():
                 clock = False
             
         if clock:
-            if(self.debug): print("Turn counter-clockwise")
-            motorR = 1
-            motorL = -1
-            rudder = -1
-        else:
             if(self.debug): print("Turn clockwise")
             motorR = -1
             motorL = 1
             rudder = 1
+        else:
+            if(self.debug): print("Turn counter-clockwise")
+            motorR = 1
+            motorL = -1
+            rudder = -1
 
-        marge = abs(self.goalAngle - self.boatAngle) if abs(self.goalAngle - self.boatAngle) < 180 else 360 - abs(self.goalAngle - self.boatAngle)       
+        # marge = abs(self.goalAngle - self.boatAngle) if abs(self.goalAngle - self.boatAngle) < 180 else 360 - abs(self.goalAngle - self.boatAngle)       
         
 
         if(self.debug):
@@ -113,30 +114,53 @@ class Coords():
             print("MotorL:", motorL)
             print("MotorR:", motorR)
             print("Rudder: ", rudder)
-            print("Marge:", marge)
-        return [marge, motorL, motorR, rudder]
+            print("GOAL: ", self.goal)
+            print("Coordinates: ", self.coordinates)
+        return [motorL, motorR, rudder]
 
     def sailBoat(self, marge):
-        # self.boatAngle = calcDeltaBoatAngle()
         motorL = motorR = self.max_power
         rudder = 0
 
-        if(marge <= 20):
-            motorR = self.map(marge, 0, 100, 0, 1)
-        elif(marge > 20):
-            motorL = self.map(marge, 0, 100, 0, 1)
+        if(marge > 10):
+            motorR = self.map(marge, 0, 20, 0, 1)
+        elif(marge <= 10):
+            motorL = self.map(marge, 0, 20, 0, 1)
         
-        marge = abs(self.goalAngle - self.boatAngle) if abs(self.goalAngle - self.boatAngle) < 180 else 360 - abs(self.goalAngle - self.boatAngle)       
+        # marge = abs(self.goalAngle - self.boatAngle) if abs(self.goalAngle - self.boatAngle) < 180 else 360 - abs(self.goalAngle - self.boatAngle)
 
         if(self.debug):
-            print("-----Sailing boat----")
+            if(self.coordinates[0] < self.goal[0]):
+                self.coordinates[0] = self.coordinates[0] + 0.1
+            else:
+                self.coordinates[0] = self.coordinates[0] - 0.1
+            if(self.coordinates[1] < self.goal[1]):
+                self.coordinates[1] = self.coordinates[1] + 0.1
+            else:
+                self.coordinates[1] = self.coordinates[1] - 0.1
+
+        if(self.debug):
+            print("-----Piloting boat----")
             print("Goal Angle: ", self.goalAngle)
             print("Boat angle:", self.boatAngle) 
             print("MotorL:", motorL)
             print("MotorR:", motorR)
             print("Rudder: ", rudder)
-            print("Marge:", marge)
-        return [marge, motorL, motorR, rudder]
+            print("Max power: ", self.max_power)
+            print("GOAL: ", self.goal)
+            print("Coordinates: ", self.coordinates)
+        return [motorL, motorR, rudder]
+
+    def calcMarge(self):
+        data = self.imu.get_orientation()
+        H, R, P = data.euler_angle
+        self.boatAngle = round(H/16.0, 2)
+        if(self.goalAngle is None):
+            self.goalAngle = self.calcGoalAngle()
+        marge = abs(self.goalAngle - self.boatAngle) if abs(self.goalAngle - self.boatAngle) < 180 else 360 - abs(self.goalAngle - self.boatAngle)
+        if(self.debug):
+            print("Marge: ", marge)
+        return marge
 
 # https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude/8674#8674
     def checkGoal(self, coordinates):
@@ -156,9 +180,9 @@ class Coords():
         # if(dLat < 0.00001 or dLat > -0.00001):
         #     LatInRange = True
 
-        if(dLng < 0.0001 or dLng > -0.0001):
+        if(dLng < 0.0001 and dLng > -0.0001):
             LngInRange = True
-        if(dLat < 0.0001 or dLat > -0.0001):
+        if(dLat < 0.0001 and dLat > -0.0001):
             LatInRange = True
 
         if(LatInRange and LngInRange):
@@ -169,27 +193,34 @@ class Coords():
     def run(self):
         # Keep adjusting the boat using the callback until running is set to false
         while self.running:
-            print('while')
             if self.cb is not None:
-                rotateValues = self.rotateBoat()
-                driveValues = rotateValues
-                marge = rotateValues[0]
-                if(marge < 20):
-                    speedValues = self.sailBoat(marge)
-                    driveValues = speedValues
-                if(checkGoal(self.goal)):
-                    driveValues = [0,0,0,0]
-                    goalNumber = goalNumber + 1
-                    if(len(GOAL) < goalNumber):
-                        self.setGoal(GOAL[goalNumber])
+                if(self.connected):
+                    if(self.debug):
+                        os.system('cls')
+                    marge = self.calcMarge()
+                    if(marge < 20):
+                        speedValues = self.sailBoat(marge)
+                        driveValues = speedValues
                     else:
-                        if(self.debug):
-                            print("Destination reached")
-                        self.stop()
-                sleep(1)
-                self.cb(driveValues[1], driveValues[2], driveValues[3])
+                        rotateValues = self.rotateBoat()
+                        driveValues = rotateValues
+                    if(self.checkGoal(self.goal)):
+                        driveValues = [0,0,0]
+                        self.goalNumber = self.goalNumber + 1
+                        if(self.goalNumber <= len(self.totalGoal)):
+                            self.setGoal(self.totalGoal[self.goalNumber])
+                            self.goalAngle = self.calcGoalAngle()
+                        elif(self.running):
+                            if(self.debug):
+                                print("Destination reached")
+                            self.stop()
+                    sleep(1)
+                    self.cb(driveValues[0], driveValues[1], driveValues[2])
+                else:
+                    self.cb(0,0,0)
             else:
                 print('cb is none')
+            sleep(1)
         print('running:', self.running)
 
     def start(self):
@@ -206,20 +237,20 @@ class Coords():
     def stop(self):
         if self.t and self.running:
             self.running = False
-            self.t.join()
+            # self.t.join()
             self.t = None
             if(self.debug): print("followCoords stop")
         else:
             if(self.debug): print("followCoords not running")
 
 def foo(x,y,z):
-    print(x,y,z)
+    pass
 
 def test():
     c = Coords(foo, 50, GOAL, debug = True)
     c.setPosition(START_COORD)
     c.start()
-    while True:
+    while c.running:
         pass
 
-#test()
+# test()
